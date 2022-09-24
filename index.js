cf164bc945ae78c10cbb920be674d8c78895bd7a
Shalinda Suresh <shalinda1990@gmail.com>
2022-09-24 11:46:23 +0530

    Add more meaningful comments

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 // Requiring modules
 const express = require('express');
 const app = express();
+//Let express app to know we use ejs template for render frontend
 app.set('view engine', 'ejs');
-const ejs = require('ejs');
-var fs = require('fs');
+const ejs = require('ejs');;
+//Use port number 8000
 const port = 8000;
 
 app.get('/', function (req, res) {
@@ -14,6 +15,7 @@ app.get('/', function (req, res) {
 	ejs.renderFile('./home.ejs', {},
 		{}, function (err, template) {
 		if (err) {
+			//Throw errors if rendering ejs failed
 			throw err;
 		} else {
 			res.end(template);
@@ -21,7 +23,7 @@ app.get('/', function (req, res) {
 	});
 });
 
-// Server setup
+// Our app is started and listening on port 8000
 app.listen(port, function (error) {
 	if (error)
 		throw error;
